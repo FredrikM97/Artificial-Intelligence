0fb48c59e8859cb903ad6f673818d8b87f9bb079
Fredrik <fredrik9779@gmail.com>
2019-11-22 15:23:20 +0100

    Modify BFS algorithm

--- Labs/Lab2/Default_code/algorithms/BFS.py
+++ Labs/Lab2/Default_code/algorithms/BFS.py
@@ -11,6 +11,8 @@ class Queue:
         heapq.heappush(self.elements,(priority,item))
     def remove(self):
         return heapq.heappop(self.elements)[1]
+    def printArray(self):
+        return self.elements
 
 class bfs:
     def __init__(self,start, goal):
@@ -23,22 +25,27 @@ class bfs:
 
     def addNode(self,parent, node, cost):
         if not node in self.node:
-            self.createNode(node)
+            self.createNode(node, parent, cost)
         else:
             # Only update parent and cost if new cost is better
-            if self.node[node][cost] < cost:
-                self.node[node][parent] = parent
-                self.node[node][cost] = cost
+            if self.node[node]['cost'] > cost:
+                self.node[node]['parent'] = parent
+                self.node[node]['cost'] = cost
+                
     def getNode(self, node):
         if not node in self.node:
-            return self.createNode(node)
+            return self.createNode(node, 0, 0)
         else:
             return self.node[node]
 
-    def createNode(self,node):
+    def exists(self, node):
+        return node in self.node
+
+    def createNode(self,node, parent, cost):
+        #print("Creating node: ", node," Parent: ", parent)
         self.node[node] = {
-                'parent':0,
-                'cost':0
+                'parent':parent,
+                'cost':cost
             }
         return self.node[node]
 
@@ -54,36 +61,47 @@ def search(map, start, goal):
     frontier = Queue() # Remember steps
     # add starting cell to open list
     frontier.add(start,0)
-    
+    #print("Frontier array after add: ", frontier.printArray())
+
     # if there is still nodes to open
     while not frontier.isEmpty():
         current = frontier.remove()
         #print("temp ", type(current), "temp2 ", type(robot.goal))
         # check if the goal is reached
         if np.array_equal(current,robot.goal):
+            print("Found the goal!!")
             break
 
         # for each neighbour of the current cell
         # Implement get_neighbors function (return nodes to expand next)
         # (make sure you avoid repetitions!) (if less value -> replace Otherwise dont do anything)
-        print("Current parent: ", current)
+        #print("Frontier array after add: ", frontier.printArray())
+        #print("Visited nodes: ", robot.node )
+        #print("Get Parent: ", robot.getNode(current))
+        #print("Current parent: ", current)
         
-        for next in get_neighbors(list(current),map).items():
-            print(next)
+        for val in get_neighbors(list(current),map).items():
+            nextKey = val[0]
+            nextObj = val[1]
+
+            #print("Next key: ",nextKey, "NextObj: ", nextObj)
             # check if blocked
-            if next[1] == -1:
+            if nextObj == -1:
                 continue
 
 
             # compute cost to reach next cell
             # Implement cost function
             # Get the cost of the parent in order to decide 
-            cost = robot.getNode(current)['parent'] +1 #cost_function()
+            
+            cost = robot.getNode(current)['cost'] +1 #cost_function()
             
             # add next cell to open list
-            frontier.add(next, cost)
+            if not robot.exists(nextKey):
+                frontier.add(nextKey, cost)
+
             # add to path
-            robot.addNode(current,next[0], cost) 
+            robot.addNode(current,nextKey, cost) 
 
     return robot.node
 
@@ -92,29 +110,37 @@ def get_neighbors(current,map):
     # Get the neighbors around
     # If -1 then remember to not check that path
     #dataPoint[[]]
-    if len(current) >= 3:
-        x = current[0][0]
-        y = current[0][1] 
-    else:
-        x = current[0]
-        y = current[1]
-    print("dis is current ", len(current), " " , current)
+    #print("Length of current ", len(current), "  Data in current: " , current)
+    x = current[0]
+    y = current[1]
+    
     
     info = {}
     # Get all the items from the four directions
     # Left side
+    '''
     if x-1 >= 0: 
-        info[int2str(x-1,y)] = map[[x-1],[y]][0]
+        info[int2str(x-1,y)] = mappedValue(map,x-1,y)
     # Right side
     if x+1 < len(map[0]):
-        info[int2str(x+1,y)] = map[[x+1],[y]][0]
+        info[int2str(x+1,y)] = mappedValue(map,x+1,y)
     # Top side
     if y+1 < len(map):
-        info[int2str(x,y+1)] = map[[x],[y+1]][0]
+        info[int2str(x,y+1)] = mappedValue(map, x,y+1)
     # Bottom side
     if y-1 >= 0:
-        info[int2str(x,y-1)] = map[[x],[y-1]][0]
+        info[int2str(x,y-1)] = mappedValue(map, x,y-1)
+    '''
+    paths = [(x-1,y), (x+1,y),(x,y+1), (x,y-1)]
+    maplen = len(map)
+    
+    for pos in paths:
+        con1 = pos[0] >= 0 and pos[1] >= 0
+        con2 = pos[0] < maplen and pos[1] < maplen
+        if  con1 and con2:
+            info[pos] = mappedValue(map, pos[0],pos[1])
 
+    #print("Current data in info: ", info)
     return info
 
 # Do dis
@@ -122,6 +148,9 @@ def cost_function():
     pass
 
 def int2str(val1,val2):
-    return str(val1)+ ","+str(val2)
+    return tuple([val1,val2])
+    
+def mappedValue(map, x, y):
+    return int(map[[x],[y]][0])
     
         
--- Labs/Lab2/Default_code/main.py
+++ Labs/Lab2/Default_code/main.py
@@ -4,9 +4,20 @@ import numpy as np
 from algorithms import BFS
 
 _map_ = pp.generateMap2d([60,60])
-plt.clf()
-plt.imshow(_map_)
+#plt.clf()
+#plt.imshow(_map_)
 
-start, goal = list(np.argwhere(_map_ == -2)[0]), list(np.argwhere(_map_ == -3)[0])
-print(start)
-BFS.search(_map_, start, goal)
+start, goal = tuple(np.argwhere(_map_ == -2)[0]), tuple(np.argwhere(_map_ == -3)[0])
+print("Start value: ",start)
+nodes = BFS.search(_map_, start, goal)
+#print(nodes)
+algorithm_map = _map_
+
+for keys in nodes.items():
+    algorithm_map[keys[0][0]][keys[0][1]] = keys[1]['cost']
+
+algorithm_map[goal[0]][goal[1]]=-10
+algorithm_map[start[0]][start[1]]=-5
+print(algorithm_map)
+plt.imshow(algorithm_map)
+plt.show()
