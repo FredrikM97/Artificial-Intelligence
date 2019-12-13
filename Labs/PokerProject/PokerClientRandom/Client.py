97d8257cd65ffcf20d2b1f60b5ba3d768c7320a2
Fredrik <fredrik9779@gmail.com>
2019-12-13 19:53:46 +0100

    Improved phases and added glitches

--- Labs/PokerProject/PokerClientRandom/Client.py
+++ Labs/PokerProject/PokerClientRandom/Client.py
@@ -1,84 +1,144 @@
-__author__ = 'fyt'
-
 import socket
 import random
 import ClientBase
 
 class Agent:
-    def __init__(self, name, hand):
+    def __init__(self, name):
         self.name = name
         self.Chips = 0
-        self.currentBet = 0
-        self.CurrentHand = hand
+        self.CurrentBet = 0
         self.Ante = 0
-        self.playersCurrentBet = 0
-
-    def queryOpenAction(self, minimumPotAfterOpen, playersCurrentBet, playersRemainingChips):
-        '''
-        * Called during the betting phases of the game when the player needs to decide what open
-        * action to choose.
-        '''
-        raise NotImplementedError
-
-    def queryCallRaiseAction(self, maximumBet, minimumAmountToRaiseTo, playersCurrentBet, playersRemainingChips):
-        raise NotImplementedError
-
-    def queryCardsToThrow(self, _hand):
-        raise NotImplementedError
-
-class RandomAgent(Agent):
-    def __init__(self, name="Random", hand=[], ip='127.0.0.1', port=5000):
-        Agent.__init__(self, name, hand)
-        # IP address and port
-        self.IP = ip
-        self.PORT = port
-        self.BUFFER_SIZE = 1024
+        self.CurrentHand = []
+        self.maxBet = 1
+        self.minRaise = 1
 
     '''
     * Called during the betting phases of the game when the player needs to decide what open
     * action to choose.
     '''
-    def queryOpenAction(self, _minimumPotAfterOpen, _playersCurrentBet, _playersRemainingChips):
-        print("Player requested to choose an opening action.")
-       
-        def openAction():    
-            if _playersCurrentBet + _playersRemainingChips > _minimumPotAfterOpen:
-                #return ClientBase.BettingAnswer.ACTION_OPEN,  iOpenBet
-                return ClientBase.BettingAnswer.ACTION_OPEN,  (random.randint(0, 10) + _minimumPotAfterOpen) if _playersCurrentBet + _playersRemainingChips + 10> _minimumPotAfterOpen else _minimumPotAfterOpen
-            else:
-                return ClientBase.BettingAnswer.ACTION_CHECK
-
-        return {
-            0: ClientBase.BettingAnswer.ACTION_CHECK,
-            1: ClientBase.BettingAnswer.ACTION_ALLIN,
-        }.get(random.randint(0, 2), openAction())
-
+    def queryOpenAction(self, minimumPotAfterOpen, playersCurrentBet, playersRemainingChips):
+        raise NotImplementedError
    
     '''
     * Called during the betting phases of the game when the player needs to decide what call/raise
     * action to choose.
     '''
-    def queryCallRaiseAction(self, _maximumBet, _minimumAmountToRaiseTo, _playersCurrentBet, _playersRemainingChips):
-        print("Player requested to choose a call/raise action.")
-
-        def callRaise():
-            # Random Open Action
-            if  _playersCurrentBet + _playersRemainingChips > _minimumAmountToRaiseTo:
-                return ClientBase.BettingAnswer.ACTION_RAISE,  (random.randint(0, 10) + _minimumAmountToRaiseTo) if _playersCurrentBet+ _playersRemainingChips + 10 > _minimumAmountToRaiseTo else _minimumAmountToRaiseTo
-            else:
-                return ClientBase.BettingAnswer.ACTION_FOLD
-    
-        return {
-            0: ClientBase.BettingAnswer.ACTION_FOLD,
-            1: ClientBase.BettingAnswer.ACTION_ALLIN,
-            2: ClientBase.BettingAnswer.ACTION_CALL if _playersCurrentBet + _playersRemainingChips > _maximumBet else ClientBase.BettingAnswer.ACTION_FOLD
-        }.get(random.randint(0, 3), callRaise())
+    def queryCallRaiseAction(self, maximumBet, minimumAmountToRaiseTo, playersCurrentBet, playersRemainingChips):
+        raise NotImplementedError
 
     '''
     * Strategy to throw cards
     '''
     def queryCardsToThrow(self, _hand):
+        raise NotImplementedError
+    
+    def bet(self,number):
+        assert number <= self.Chips
+        self.Chips -= number
+        self.CurrentBet += number
+   
+    def openAction(self, bet=0, **kwargs):
+        self.bet(bet)
+
+    def checkAction(self, **kwargs):
+        pass # yes, do nothing
+
+    def allinAction(self, **kwargs):
+       self.bet(self.Chips)
+        
+    def foldAction(self, **kwargs):
+        self.CurrentBet = 0
+
+    def callAction(self, _maximumBet=0, _playersCurrentBet=0, **kwargs):
+        diff = _maximumBet - _playersCurrentBet
+        self.bet(diff)
+
+    def raiseAction(self, bet=0, **kwargs):
+        self.bet(bet)
+        
+
+class RandomAgent(Agent):
+    def __init__(self, name="Random", hand=[], ip='127.0.0.1', port=5000):
+        Agent.__init__(self, name)
+        # IP address and port
+        self.IP = ip
+        self.PORT = port
+        self.BUFFER_SIZE = 1024
+
+    def queryOpenAction(self, _minimumPotAfterOpen, _playersCurrentBet, _playersRemainingChips):
+        print("Player requested to choose an opening action.")
+       
+        def canOpen():    
+            if _playersCurrentBet + _playersRemainingChips > _minimumPotAfterOpen:
+                if _playersCurrentBet + _playersRemainingChips + 10> _minimumPotAfterOpen: 
+                    bet = (random.randint(0, 10) + _minimumPotAfterOpen)
+                else: 
+                    bet = _minimumPotAfterOpen
+                return ClientBase.BettingAnswer.ACTION_OPEN, bet
+            else:
+                return ClientBase.BettingAnswer.ACTION_CHECK
+            
+        action = {
+            0: ClientBase.BettingAnswer.ACTION_CHECK,
+            1: ClientBase.BettingAnswer.ACTION_ALLIN,
+        }.get(random.randint(0, 2), canOpen())
+
+        switch = {
+            ClientBase.BettingAnswer.ACTION_OPEN:self.openAction,
+            ClientBase.BettingAnswer.ACTION_CHECK:self.checkAction, 
+            ClientBase.BettingAnswer.ACTION_ALLIN:self.allinAction
+        }
+
+        if issubclass(action.__class__,str):
+            switch[action]()  
+        else: 
+            switch[action[0]](bet=action[1])
+
+        return action
+    
+
+    def queryCallRaiseAction(self, _maximumBet, _minimumAmountToRaiseTo, _playersCurrentBet, _playersRemainingChips):
+        print("Player requested to choose a call/raise action.")
+
+        def canRaise():
+            # If can raise
+            if  _playersCurrentBet + _playersRemainingChips > _minimumAmountToRaiseTo:
+                # do raise
+                if _playersCurrentBet+ _playersRemainingChips + 10 > _minimumAmountToRaiseTo: 
+                    bet = (random.randint(0, 10) + _minimumAmountToRaiseTo) 
+                else: 
+                    bet = _minimumAmountToRaiseTo
+                return ClientBase.BettingAnswer.ACTION_RAISE, bet
+            else:
+                return ClientBase.BettingAnswer.ACTION_FOLD
+
+        action =  {
+            0: ClientBase.BettingAnswer.ACTION_FOLD,
+            1: ClientBase.BettingAnswer.ACTION_ALLIN,
+            2: ClientBase.BettingAnswer.ACTION_CALL if _playersCurrentBet + _playersRemainingChips > _maximumBet else ClientBase.BettingAnswer.ACTION_FOLD
+        }.get(random.randint(0, 3), canRaise())
+
+        switch = {
+            ClientBase.BettingAnswer.ACTION_FOLD:self.foldAction,
+            ClientBase.BettingAnswer.ACTION_CALL:self.callAction, 
+            ClientBase.BettingAnswer.ACTION_ALLIN:self.allinAction,
+            ClientBase.BettingAnswer.ACTION_RAISE:self.raiseAction
+        }
+
+        args={'_maximumBet':_maximumBet,'_playersCurrentBet':_playersCurrentBet}
+        if issubclass(action.__class__,str):
+            switch[action](**args)  
+        else: 
+            switch[action[0]]({'bet':action[1],**args})
+
+        return action
+
+    def updateAgent(self, currentBet):
+        self.CurrentBet = currentBet
+        self.Chips -= currentBet
+
+    def queryCardsToThrow(self, _hand):
         print("Requested information about what cards to throw")
         print(_hand)
-        return _hand[random.randint(0,4)] + ' '
+        return _hand[random.randint(0,4)] #+ ' '
 
--- Labs/PokerProject/PokerClientRandom/PokerGame.py
+++ Labs/PokerProject/PokerClientRandom/PokerGame.py
@@ -15,15 +15,14 @@ from game.serverInfo import *
 def main():
     print("Starting game.. Waiting for server")
     # Make client1
-    myClient1 = client(RandomAgent('Subject1.0', port=5000), True)
+    myClient1 = client(RandomAgent('Subject1.0', ip='127.0.0.1',port=5000), True)
     t1 = Thread(target=myClient1.run)
-    
 
     # Make client2
     myClient2 = client(RandomAgent('Subject2.0', ip='127.0.0.2',port=5000), False)
     t2 = Thread(target=myClient2.run)
     
-    # Make client2
+    # Make client3
     myClient3 = client(RandomAgent('Subject3.0', ip='127.0.0.3',port=5000), False)
     t3 = Thread(target=myClient3.run)
     
@@ -45,61 +44,37 @@ class client:
         # Declare constants
         self.SIGNAL_ALIVE = '==================ALIVE======================' 
         self.iMsg = 0
-        
-    def send_but_working(self,string): # use this when doing python 3+
-        self.s.send(str.encode(string + '\n'))
+        self.gameFlow = ['info','open','betting','draw','betting']
+        self.phaseIndex = 0
 
-    # Return agent name to Server
-    def handle_name(self, *_, agent=None, MsgFractions=[], **kwargs):
-        self.send_but_working('Name ' + agent.name)
-    
+    '''
+    ***** Get data from server *****
+    '''    
     # Get how much chips agent has, or print opponents chips
     def handle_Chips(self, *_, agent=None, MsgFractions=[], **kwargs):
         if MsgFractions[0] == agent.name:
             agent.Chips = int(MsgFractions[1])
         else:
-            infoPlayerChips(MsgFractions, **kwargs) #TODO
+            infoPlayerChips(MsgFractions, **kwargs) 
 
     # Get Ante to agent, and print it
     def handle_Ante_Changed(self, *_, agent=None, MsgFractions=[], **kwargs):
         agent.Ante = int(MsgFractions[0])
-        infoAnteChanged(MsgFractions, **kwargs)#TODO
+        infoAnteChanged(MsgFractions, **kwargs)
 
-    # Force a bet if agents turn
-    def handle_Forced_Bet(self, *_, agent=None, MsgFractions=[], **kwargs):
-        if MsgFractions[0] == agent.name:
-            agent.playersCurrentBet = agent.playersCurrentBet + int(MsgFractions[1])
-        else:
-            infoForcedBet(MsgFractions, **kwargs)#TODO
+    # Get Raised to agent, and print it
+    def handle_Info_Raise(self, *_, agent=None, MsgFractions=[], **kwargs):
+        oldmax = agent.maxBet
+        agent.maxBet = int(MsgFractions[1])
+        agent.minRaise = agent.maxBet - oldmax
+        infoPlayerRise(MsgFractions, **kwargs)
 
-    # Open bet at beginning depending on Agent
-    def handle_Open(self, *_, agent=None, MsgFractions=[], **kwargs):
-        intFractions = [int(Fraction) for Fraction in MsgFractions]
-        minimumPotAfterOpen, playersCurrentBet, playerRemainingChips = intFractions
-        tmp = agent.queryOpenAction(*intFractions)
+    # Get open bet to agent, and print it
+    def handle_Info_Player_Open(self, *_, agent=None, MsgFractions=[], **kwargs):
+        agent.maxBet = int(MsgFractions[1])
+        agent.minRaise = 1
+        infoPlayerOpen(MsgFractions, **kwargs)
 
-        # For check and All-in/ Open
-        to_send = tmp if issubclass(tmp.__class__, str) else ' '.join([str(item) for item in tmp])
-        print("Open:",to_send)
-        self.send_but_working(to_send)
-
-        print(f'{self.SIGNAL_ALIVE} \n {agent.name} Action: {tmp}')
-
-    # Do a call or raise depending on Agent
-    def handle_Call_or_Raise(self, *_, agent=None, MsgFractions=[], **kwargs):
-        intFractions = [int(Fraction) for Fraction in MsgFractions]
-        maximumBet, minimumAmountToRaiseTo, playersCurrentBet, playersRemainingChips = intFractions
-        tmp = agent.queryCallRaiseAction(*intFractions)
-        
-
-        # For fold, all-in, call / For raise
-        to_send = tmp if issubclass(tmp.__class__,str) else ' '.join([str(item) for item in tmp])
-        print("Call:",to_send)
-        self.send_but_working(to_send)
-
-        print(f'{self.SIGNAL_ALIVE} \n {agent.name} Action: {tmp}')
-
-    # Give hand to agent
     # Give hand to agent
     def handle_Cards(self, *_, agent=None, MsgFractions=[], **kwargs):
         #infoCardsInHand(MsgFractions) # show info for hands
@@ -109,49 +84,71 @@ class client:
         infoPlayerHand((agent.name, *MsgFractions), **kwargs)
         #print('CurrentHand>', agent.CurrentHand)
 
+    '''
+    ***** ACTIONS to server *****
+    '''    
+    def send_but_working(self,string): # use this when doing python 3+
+        print('Current phase:', self.phase, 'Message:',string)
+        self.nextPhase()
+        self.s.send(str.encode(string + '\n'))
+        
+    # Return agent name to Server
+    def handle_name(self, *_, agent=None, MsgFractions=[], **kwargs):
+        self.send_but_working('Name ' + agent.name)
+        self.phase = 'info'
+        self.phaseIndex = 0
+
+    # Force a bet if agents turn
+    def handle_Forced_Bet(self, *_, agent=None, MsgFractions=[], **kwargs):
+        if MsgFractions[0] == agent.name:
+            agent.CurrentBet += int(MsgFractions[1])
+        else:
+            infoForcedBet(MsgFractions, **kwargs)
+
+    # Open bet at beginning depending on Agent
+    def handle_Open(self, *_, agent=None, MsgFractions=[], **kwargs):
+        intFractions = [int(Fraction) for Fraction in MsgFractions]
+        # minimumPotAfterOpen, CurrentBet, playersRemainingChips = intFractions
+        tmp = agent.queryOpenAction(*intFractions)
+
+        # For check and All-in/ Open
+        to_send = tmp if issubclass(tmp.__class__, str) else ' '.join([str(item) for item in tmp])
+        print("Open:",to_send)
+        self.send_but_working(to_send)
+        
+        print(f'{self.SIGNAL_ALIVE} \n {agent.name} Action: {tmp}')
+
+    # Do a call or raise depending on Agent
+    def handle_Call_or_Raise(self, *_, agent=None, MsgFractions=[], **kwargs):
+        intFractions = [int(Fraction) for Fraction in MsgFractions]
+        # maximumBet, minimumAmountToRaiseTo, CurrentBet, playersRemainingChips = intFractions
+        tmp = agent.queryCallRaiseAction(*intFractions)
+
+        # For fold, all-in, call / For raise
+        to_send = tmp if issubclass(tmp.__class__,str) else ' '.join([str(item) for item in tmp])
+        print("Call:",to_send)
+        self.send_but_working(to_send)
+        
+        print(f'{self.SIGNAL_ALIVE} \n {agent.name} Action: {tmp}')
+
     # Throw agent hand   
     def handle_Draw(self, *_, agent=None, MsgFractions=[], **kwargs):
         discardCards = agent.queryCardsToThrow(agent.CurrentHand)
         self.send_but_working('Throws ' + discardCards)
         print(agent.name, 'Action:','Throws', discardCards)
-    
+    '''
+    *****  Guess current phase of server *****
+    ''' 
     # Try to figure out which phase
     def guessPhase(self,agent,RequestType):
-        '''
-        Rules:
-        * While not in current -> next
-        * Many steps in while loop is bad,missed to predict -> for reinforced learning 
-
-        * Prephase
-            - won (if not first round)
-            - Round
-            - chips
-            - hand
-            - ante
-            - forced bet
-        * Opening
-            - open
-            - check
-            - all-in
-        * Betting
-            - Call
-            - fold
-            - all-in
-            - raise 
-        '''
         # black magic fuckery to save us from creating the sets every call
         if 'phase2set' not in self.guessPhase.__dict__:
-            # make stuff
-            nextFace = {'info':'open','open':'betting','betting':'info'}
-            self.guessPhase.__dict__.update({'nextFace':nextFace})
-
             infoSet = {
                 'Name?',
                 'Chips',
                 'Ante_Changed',
                 'Forced_Bet',   
                 'Cards',
-                'Draw',
                 'Round',
                 'Game_Over',
                 'Player_Draw',
@@ -177,33 +174,62 @@ class client:
                 'unknown_action'
             }
 
+            drawSet = {
+                'Draw?'
+            }
+
             phase2set = {
                 'info':infoSet, 
                 'open':openSet, 
-                'betting':betttingSet}
+                'betting':betttingSet,
+                'draw':drawSet
+            }
             self.guessPhase.__dict__.update({'phase2set':phase2set})
         else:
-            nextFace = self.guessPhase.__dict__['nextFace']
             phase2set = self.guessPhase.__dict__['phase2set']
 
         # While not in current -> next
+        print('Type:',RequestType, "Phasor:",self.phase)
         while RequestType not in phase2set[self.phase]:
-            self.phase = nextFace[self.phase]
+            self.nextPhase()
             if self.hawkeye:
-                print("Request:", RequestType)
-                print('Agent phase:', self.phase)
-        
-        
+                print("Request:", RequestType, 'Agent phase:', self.phase)
+
+    
+    def nextPhase(self):
+        self.phaseIndex += 1
+        self.phaseIndex %= len(self.gameFlow)
+        self.phase = self.gameFlow[self.phaseIndex]
+
     # Do action based on phase
-    def guessAction(self, msg, **kwarg):
-        # actions = ['fold','all-in','call','check','raise', 'open']
+    def guessAction(self, msg, agent=None, MsgFractions=[], **kwarg):
+        agent = self.agent if agent is None else agent # use local agent if none
+        
         phase2action={
             'info':lambda *arg,**kwarg:None, 
             'open':self.handle_Open, 
-            'betting':self.handle_Call_or_Raise
+            'betting':self.handle_Call_or_Raise,
+            'draw':self.handle_Draw
         }
-        phase2action[self.phase](msg, **kwarg)
+        #TODO: do the all in case
+        minimumPotAfterOpen = agent.Ante * 5 # TODO: reeee member it's 5 as in 5 players
+        CurrentBet = agent.CurrentBet
+        maximumBet = agent.maxBet
+        minimumAmountToRaiseTo = maximumBet + agent.minRaise
+        playersRemainingChips = agent.Chips
 
+        phase2msg = {
+            'info':[], 
+            'open':[minimumPotAfterOpen, CurrentBet, playersRemainingChips], 
+            'betting':[maximumBet, minimumAmountToRaiseTo, CurrentBet, playersRemainingChips],
+            'draw':[]
+        }
+
+        phase2action[self.phase](agent=agent, MsgFractions=phase2msg[self.phase], **kwarg)
+        
+    '''
+    ***** Run *****
+    ''' 
     def run(self):
         infoTablets = {
             'Name?':self.handle_name,
@@ -213,12 +239,12 @@ class client:
             'Open?':self.handle_Open,
             'Call/Raise?':self.handle_Call_or_Raise,
             'Cards':self.handle_Cards,
-            'Draw':self.handle_Draw,
+            'Draw?':self.handle_Draw,
             'Round':infoNewRound,
             'Game_Over':infoGameOver,
-            'Player_Open':infoPlayerOpen,
+            'Player_Open':self.handle_Info_Player_Open,
             'Player_Check':infoPlayerCheck,
-            'Player_Raise':infoPlayerRise,
+            'Player_Raise':self.handle_Info_Raise,
             'Player_Call':infoPlayerCall,
             'Player_Fold':infoPlayerFold,
             'Player_All-in':infoPlayerAllIn,
@@ -228,7 +254,7 @@ class client:
             'Player_Hand':infoPlayerHand,
             'Result':infoResult,
             'unknown_action':self.guessAction
-            }
+        }
 
         while True:
             try:
