141483a6840d1767e64885a06aaa0e800f6ab073
thunderoux <fredrik9779@gmail.com>
2019-12-15 13:36:42 +0100

    Small fixes

---
+++ Labs/PokerProject/PokerClientRandom/setup.py
@@ -0,0 +1,21 @@
+'''
+python setup.py build
+python setup.py install --user
+'''
+
+"""
+Poker AI
+"""
+
+from setuptools import setup
+#[metadata]
+setup(
+    name='PokerAI',
+    version='0.1',
+    description=__doc__,
+    #long_description=open('README.md').read(),
+    author='Fredrik Mårtensson, Jesper Holmblad',
+    url='https://github.com/FredrikM97/Artificial-Intelligence/tree/master/Labs/PokerProject/PokerClientRandom',
+    packages=['game', 'evolution'],
+    install_requires = ['deuces @ https://github.com/FredrikM97/deuces']
+)
