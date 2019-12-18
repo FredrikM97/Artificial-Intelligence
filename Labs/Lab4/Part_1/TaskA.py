ca67807229be70ef7357ff39b32623221e5e7384
Fredrik <fredrik9779@gmail.com>
2019-12-18 19:26:38 +0100

    TODO list added

--- Labs/Lab4/Part_1/TaskA.py
+++ Labs/Lab4/Part_1/TaskA.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from collections import Counter
@@ -9,20 +8,16 @@ def main():
     
     # Create training and test set
     Train_set, Test_set = train_test_split(data, test_size=0.2)
-    Input_train = Train_set#[:, :8]
-    #Target_train = Train_set[:, 9]
-    Input_test = Test_set#[:, :8]
-    #Target_test =  Test_set[:, 9]
     
-    print("Test rows:",)
+    # Training set, testset with amount of rows, and K
     classifier = prediction(Train_set, Test_set[:5],3)
     target = [i for *_,i in Test_set[:5]]
     print("Prediction/Correct Answer:",list(zip(classifier,target)))
     '''
     TODO:
     * Calculate accuracy
-    * Try to plot
-    
+    * Try to plot (Maybe not)
+    * 
     '''
 
 def knn(train, test_row, k):
@@ -34,7 +29,7 @@ def prediction(train, test_set, k):
     classes = []
     for test_row in test_set:
         neighbors = knn(train, test_row, k)
-        derp = [i for *_,i in neighbors]
+        derp = [i for *_,i in neighbors] # Change to extract different columns
         classes.append(model(derp,0))
     return classes
 
@@ -50,8 +45,6 @@ def euclidean(pos1, pos2):
         diff = (x - y)
         sum += diff*diff
     return (sum)**.5
-
-
     
 if __name__ == "__main__":
     main()
