Fixed crossvalidation and normalization

Labs/Lab4/Part_1/TaskC_G.py
@@ -1,7 +1,8 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier,KNeighborsRegressor
 from operator import itemgetter
+from sklearn.preprocessing import normalize
 
 '''
 Task 1 “C”: Parameter tuning
@@ -9,7 +10,7 @@ Task 1 “C”: Parameter tuning
 neighbors and change the distance measurement method.
 '''
 def main():
-    neighbors = [1,2,3,4,5,6,7,8]
+    k_list = list(range(1,15))
     metrics = [
         ('euclidean'),
         ('manhattan'),
@@ -18,12 +19,16 @@ def main():
     classifiers = [KNeighborsClassifier,KNeighborsRegressor]
     data = np.loadtxt(open('Part_1/Lab4Data.csv', "rb"), delimiter=";", skiprows=1)
 
-
+    np.random.seed(420)
     Train_set, Test_set = train_test_split(data, test_size=0.2)
+    
+    '''
+    TODO: FIX CROSSVALIDATION
+    '''
 
-    Input_train = Train_set[:, :8]
+    Input_train = normalizeData(Train_set[:, :8])
     Target_train = Train_set[:, 9]
-    Input_test = Test_set[:, :8]
+    Input_test = normalizeData(Test_set[:, :8])
     Target_test =  Test_set[:, 9]
 
     print('Training:', len(Train_set),'Testing:', len(Test_set))
@@ -32,18 +37,31 @@ def main():
     for classifier in classifiers:
         statistics = []
         
-        for neighbor in neighbors:
+        for k in k_list:
             for metric in metrics:
-                classy = classifier(neighbor, metric=metric)
+                classy = classifier(k, metric=metric)
                 model = classy.fit(Input_train, Target_train)
                 PredictedOutcome = model.predict(Input_test)
                 Number_of_Correct_Predictions = len([i for i, j in zip(PredictedOutcome, Target_test) if i == j])
                 accuracy = (Number_of_Correct_Predictions/float(len(PredictedOutcome)))*100
-                statistics.append((neighbor,metric, round(float(accuracy),2)))
+                scores = cross_val_score(model, Input_train, Target_train, cv=5) # Cross validation
+                statistics.append((k,metric, round(float(accuracy),2),scores))
+                
+
 
         print("\nClassifier:",str(classifier.__name__))
-        for (neighboor, name, accuracy) in sorted(statistics, key = lambda x: x[2], reverse=True):
-            print(name, ":", neighboor, ":",accuracy,"%")
-            
+        for (neighboor, name, accuracy, scores) in sorted(statistics, key = lambda x: x[2], reverse=True):
+            print(name, ":", neighboor, ":",accuracy,"%", "Crossval:",scores)
+
+# Just for fun
+def normalizeData(data):
+    normData = []
+    for col in zip(*data):
+        high = max(col)
+        low = min(col)
+        col = [item-low/high for item in col]
+        normData.append(col)
+    return [row for row in zip(*normData)]
+
 if __name__ == "__main__":
     main()